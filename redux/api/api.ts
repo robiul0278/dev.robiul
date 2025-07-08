fb36881668e777e5ccedb8c3847e55c1e14c7bd6
robiul0278 <robiul0278@gmail.com>
2025-07-08 16:54:35 +0600

    update delete api

--- redux/api/api.ts
+++ redux/api/api.ts
@@ -4,7 +4,7 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 export const baseApi = createApi({
   reducerPath: 'baseApi',
   // baseQuery: fetchBaseQuery({ baseUrl: 'http://localhost:5000/api/v1' }),
-  baseQuery: fetchBaseQuery({ baseUrl: 'https://dev-robiul-server.vercel.app/api/v1' }),
+  baseQuery: fetchBaseQuery({ baseUrl: 'https://dev-robiul-server.vercel.app/api/v1'}),
   tagTypes: ["project"],
   endpoints: (builder) => ({
     createProject: builder.mutation({
